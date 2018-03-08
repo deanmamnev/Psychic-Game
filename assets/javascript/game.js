12238b70a8a79bac8acf87fb87bbd6ddb8ecf199
Dean Mamnev <deanmamnev@gmail.com>
2018-03-08 09:59:50 -0600

    Here we go again...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+
 // create array of all possible choices[done]
 
 alphabet = [];
@@ -8,31 +9,61 @@ console.log(alphabet);
 
 // create array of possible Answers [done]
 
-computerChoices = [alphabet];
+var computerChoices = alphabet;
 
+var computerGuess;
+
+var wins = 0;
+
+var losses =0;
+
+var guessesLeft = 9;
+
+var guessesSoFar = 0;
 // Randomize the Answer choices [NEEDS FIXING]
 
-function randomchoice() {
-    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-console.log(computerGuess)
-}
-
+console.log("call randomchoice");
 randomchoice();
-console.log()
+
 
 // This function is run whenever the user presses a key. [done]
 document.onkeyup = function (event) {
 
     // Determines which key was pressed. [done]
     var userGuess = event.key;
-    console.log(userGuess)
+    console.log(userGuess+" "+computerGuess)
+    // If userchoice matches computerchoice
+    if (userGuess == computerGuess) {
+        console.log("match");
+        wins++;
 
+
+        // thanks to martin
+        //Increment wins total in HTML
+                   //Get HTML element for displaying wins
+                   var winsHTML = document.getElementById("wins");
+                   //set HTML to current wins total
+                   winsHTML.innerHTML = ("<strong>" + wins + "</strong>")
+    }
+
+    else{
+        console.log("fail");
+        losses++;
+        var lossesHTML = document.getElementById("losses");
+        lossesHTML.innerHTML = ("<strong>" + losses + "</strong>")
+    }
+}
+
+function randomchoice() {
+
+    console.log(computerChoices.length+"More text"+computerChoices[13]);
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    console.log("Computer choice is: "+computerGuess);
 }
 
 
-
-// If userchoice matches computerchoice
 // if (userGuess == computerGuess) {
 //         // game win message
 //         alert(Correct!);
@@ -47,7 +78,7 @@ document.onkeyup = function (event) {
     //failure, display failure message
 
     // loop / lives left
-    if(chances > 0)
+    // if(chances > 0)
 
         // for()
     //if lives = 0
@@ -57,4 +88,4 @@ document.onkeyup = function (event) {
 // display in html wins
 // display in html losses
 // display in html guesses left
-// display in html guesses so far
+// display in html letters guessed so far
