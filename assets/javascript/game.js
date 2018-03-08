ffe7465c4b184ddb7788b5bf2acec9f240978253
Dean Mamnev <deanmamnev@gmail.com>
2018-03-08 14:56:18 -0600

    Almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,7 +15,7 @@ var computerGuess;
 
 var wins = 0;
 
-var losses =0;
+var losses = 0;
 
 var guessesLeft = 9;
 
@@ -32,35 +32,63 @@ document.onkeyup = function (event) {
 
     // Determines which key was pressed. [done]
     var userGuess = event.key;
-    console.log(userGuess+" "+computerGuess)
+    console.log(userGuess + " " + computerGuess)
     // If userchoice matches computerchoice
     if (userGuess == computerGuess) {
         console.log("match");
         wins++;
 
 
-        // thanks to martin
+        // thanks to Martin G
         //Increment wins total in HTML
-                   //Get HTML element for displaying wins
-                   var winsHTML = document.getElementById("wins");
-                   //set HTML to current wins total
-                   winsHTML.innerHTML = ("<strong>" + wins + "</strong>")
+        //Get HTML element for displaying wins
+        var winsHTML = document.getElementById("wins");
+        //set HTML to current wins total
+        winsHTML.innerHTML = ("<strong>" + wins + "</strong>")
+
+        guessesLeft = 9;
+
+        var guessesLeftHTML = document.getElementById("guessesLeft");
+        guessesLeftHTML.innerHTML = ("<strong>" + guessesLeft + "</strong>")
+
+        guessesSoFar = 0;
+        var guessesSoFarHTML = document.getElementById("guessesSoFar");
+        guessesSoFarHTML.innerHTML = ("<strong>" + guessesSoFar + "</strong>")
+        randomchoice();
     }
 
-    else{
+    else {
         console.log("fail");
+        guessesLeft--;
+        var guessesLeftHTML = document.getElementById("guessesLeft");
+        guessesLeftHTML.innerHTML = ("<strong>" + guessesLeft + "</strong>")
+        guessesSoFar++;
+        var guessesSoFarHTML = document.getElementById("guessesSoFar");
+        guessesSoFarHTML.innerHTML = ("<strong>" + guessesSoFar + "</strong>")
+    }
+    if (guessesLeft == 0) {
+        console.log("game over");
         losses++;
         var lossesHTML = document.getElementById("losses");
         lossesHTML.innerHTML = ("<strong>" + losses + "</strong>")
+        guessesLeft = 9;
+        guessesSoFar = 0;
+        var guessesLeftHTML = document.getElementById("guessesLeft");
+        guessesLeftHTML.innerHTML = ("<strong>" + guessesLeft + "</strong>")
+
+        randomchoice();
+
     }
 }
 
+
+
 function randomchoice() {
 
-    console.log(computerChoices.length+"More text"+computerChoices[13]);
+    console.log(computerChoices.length + "More text" + computerChoices[13]);
     computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-    console.log("Computer choice is: "+computerGuess);
+    console.log("Computer choice is: " + computerGuess);
 }
 
 
